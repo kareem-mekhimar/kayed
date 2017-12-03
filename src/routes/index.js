7c76490d4dfb929c2cb1425ea3ce1b41c4427ce1
km <k.i.m.oo@hotmail.com>
2017-12-03 20:26:07 +0200

    qwq

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,7 @@ import authRoute from "./auth.route";
 import userRoute from "./user.route" ;
 import categoryRoute from "./category.route" ;
 import barterRoute from "./barter.route" ;
-<<<<<<< HEAD
-=======
 import barterOfferRoute from "./barter-offer.route" ;
->>>>>>> 56659be18bb9dc75fd115f32f5e613b2704d5a23
 import auctionRoute from "./auction.route" ;
 
 const requireAuth = passport.authenticate('jwt',{ session : false }) ;
@@ -16,12 +13,8 @@ const requireAuth = passport.authenticate('jwt',{ session : false }) ;
 const router = express.Router();
   
 
-<<<<<<< HEAD
 router.use("/", authRoute) ;
-=======
-router.use("/", authRoute);
 router.use("/users", requireAuth, userRoute);
->>>>>>> 56659be18bb9dc75fd115f32f5e613b2704d5a23
 router.use("/categories", requireAuth, categoryRoute) ;
 router.use("/barters", requireAuth, barterRoute);
 router.use("/auctions", requireAuth, auctionRoute) ;
