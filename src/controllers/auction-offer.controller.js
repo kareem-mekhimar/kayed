23b69488d29796a4b42f1f1831d3bc2373ebad96
km <k.i.m.oo@hotmail.com>
2017-12-04 15:13:03 +0200

    d

--- src/controllers/auction-offer.controller.js
+++ src/controllers/auction-offer.controller.js
@@ -1,13 +1,19 @@
 import AuctionOffer from "../models/auction-offer.model" ;
-
+import User from "../models/user.model";
 import ApiResponse from "../helpers/ApiResponse";
 import ApiError from "../helpers/ApiError";
 
 const validateAuctionOffer = req => {
-     
+    req.checkBody("bidder").notEmpty().withMessage("bidder Required")
+    .custom(value => {
+        return User.findById(value).then(user => {
+            if (!user)
+                throw new Error("bidder user Is Not Found");
+        })
+    }).withMessage("bidder is Not Found in the system");
 };
 
 
 export default {
-
+      
 }
