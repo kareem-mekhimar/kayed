960d9a48eb7f03666efdf9951b6d8994e827d72a
unknown <vipa00magdy00@gmail.com>
2017-12-05 21:19:49 +0200

    Add User Favourites Auctions&Barters

---
+++ src/models/fav-barter.model.js
@@ -0,0 +1,30 @@
+
+import mongoose from "mongoose" ;
+const Schema = mongoose.Schema ;
+
+const FavBarterSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId, 
+        ref: 'user',
+        required: [true, 'Id of user is required']
+    },
+    barter: {
+        type: Schema.Types.ObjectId, 
+        ref: 'barter',
+        required: [true, 'Id of barter is required']
+    },
+    creationDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+FavBarterSchema.set('toJSON', {
+    transform: function (doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    }
+}); 
+
+export default mongoose.model("fav-barter", FavBarterSchema);
