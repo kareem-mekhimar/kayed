3859cb6b6fd5020531e6e682239476f421b1befb
km <k.i.m.oo@hotmail.com>
2017-12-08 21:07:08 +0200

    sdd

--- dist/config.js
+++ dist/config.js
@@ -9,9 +9,10 @@ var config = {};
 
 //config.mongoUrl = "mongodb://localhost/nodeTests" ;
 
-//config.mongoUrl = "mongodb://admin:admin@ds123146.mlab.com:23146/kayed"
+config.mongoUrl = "mongodb://admin:admin@ds123146.mlab.com:23146/kayed";
 config.jwtSecret = "App";
-config.mongoUrl = "mongodb://localhost/kayed";
+//config.mongoUrl = "mongodb://localhost/kayed" ;
+
 
 exports.default = config;
 //# sourceMappingURL=config.js.map
