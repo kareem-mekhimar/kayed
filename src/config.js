771b922e757afe89e8894d6e78736fb23faa6e64
unknown <vipa00magdy00@gmail.com>
2017-12-08 21:52:21 +0200

    Edit UserBarters

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ const config = {};
 
 //config.mongoUrl = "mongodb://localhost/nodeTests" ;
 
-//config.mongoUrl = "mongodb://admin:admin@ds123146.mlab.com:23146/kayed"
+config.mongoUrl = "mongodb://admin:admin@ds123146.mlab.com:23146/kayed"
 config.jwtSecret = "App"  ;
-config.mongoUrl = "mongodb://localhost/kayed" ;
+//config.mongoUrl = "mongodb://localhost/kayed" ;
 
 
 export default config ;
