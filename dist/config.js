13e081650417260bc66f3617b9a261fa7a3e2ee4
km <k.i.m.oo@hotmail.com>
2017-12-11 20:24:53 +0200

    wqex

---
+++ dist/config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+
+var config = {};
+
+//config.mongoUrl = "mongodb://localhost/nodeTests" ;
+
+config.mongoUrl = "mongodb://admin:admin@ds123146.mlab.com:23146/kayed";
+config.jwtSecret = "App";
+//config.mongoUrl = "mongodb://localhost/kayed" ;
+
+
+exports.default = config;
+//# sourceMappingURL=config.js.map
