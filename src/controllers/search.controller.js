639aa636b53b8a2d8c62d4b5f9b9df79f0cea4a7
unknown <vipa00magdy00@gmail.com>
2017-12-12 03:54:20 +0200

    Reimplment search q

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose" ;
+import mongoose from "mongoose";
 import Barter from "../models/barter.model";
 import Auction from "../models/auction.model";
 import { ApiResponse2 } from "../helpers/ApiResponse"
@@ -8,50 +8,51 @@ export default {
     async search(req, res, next) {
         let { q, page, limit } = req.query;
         if (!q) {
-            res.send({ barters: [] , auctions: [] });
+            res.send({ barters: [], auctions: [] });
         }
 
-        let query = { 
+        let query = {
             $text: { $search: q }
         };
 
-        page = page ? parseInt(page) : 1;       
+        page = page ? parseInt(page) : 1;
         limit = limit ? parseInt(limit) : 10;
 
-        try{    
-                                
-            let barterCount = await Barter.count(query);        
-            let auctionCount = await Auction.count(query); 
+        try {
 
-            let minVal = Math.min(barterCount, auctionCount); 
-            let maxVal = Math.max(barterCount, auctionCount);
+            let barterCount = await Barter.count(query);
+            let auctionCount = await Auction.count(query);
 
-            let pageCount = Math.ceil(maxVal / limit) * 2 - Math.ceil( Math.abs(barterCount - auctionCount) / limit );
-            let halfLimit = limit? Math.ceil( limit/2 ) : 5;
+            let halfLimit = limit ? Math.ceil(limit / 2) : 5;
+            let pageCount = Math.ceil(barterCount + auctionCount / limit);
 
-            if(page > Math.ceil((minVal / limit) * 2))
-                halfLimit = limit;
-
-            let barters = await Barter.find(query)
-                                    .populate('relatedCategory relatedUser')
-                                    .sort({ creationDate: -1 })
-                                    .limit(halfLimit)
-                                    .skip((page - 1) * limit);
             
+            if (page == 1 && (barterCount < halfLimit || auctionCount< halfLimit))
+                halfLimit += halfLimit - Math.min(barterCount, auctionCount)
+           
+            if (page > Math.ceil((Math.min(barterCount, auctionCount) / limit) * 2 ))
+                halfLimit = limit;
+ 
+            let barters = await Barter.find(query)
+                .populate('relatedCategory relatedUser')
+                .sort({ creationDate: -1 })
+                .limit(halfLimit)
+                .skip((page - 1) * limit);
+
             let auctions = await Auction.find(query)
-                                    .populate('relatedCategory relatedUser')
-                                    .sort({ creationDate: -1})
-                                    .limit(halfLimit)
-                                    .skip((page - 1) * limit);
+                .populate('relatedCategory relatedUser')
+                .sort({ creationDate: -1 })
+                .limit(halfLimit)
+                .skip((page - 1) * limit);
 
             barters = await checkAllMyOfferAndFavouriteIn(barters, req, true);
             auctions = await checkAllMyOfferAndFavouriteIn(auctions, req, false);
 
-            let response = new ApiResponse2(undefined, page, pageCount, limit, barters.length + auctions.length,  req);
+            let response = new ApiResponse2(undefined, page, pageCount, limit, barters.length + auctions.length, req);
 
-            res.status(200).send({ barters , auctions, ...response });
-            
-        } catch(err) {
+            res.status(200).send({ barters, auctions, ...response });
+
+        } catch (err) {
             next(err);
         }
     }
